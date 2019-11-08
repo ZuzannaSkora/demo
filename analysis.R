596ac0bf50714af56952eee899030fd23cf8edff
Zuzanna Skora <zuzanna.skora@gmail.com>
2019-11-08 15:17:43 +1100

    starting

---
+++ analysis.R
@@ -0,0 +1,8 @@
+# Script for my awesome project
+
+# In R folder we have all the functions we need to run our code in this script
+# Git will only save change once I click "commit"
+# "commit" is freezing in time all previous versions
+# to send the current version to cloud (github) I have to press "push"
+# files in gitignore will not make it past R (so not going to git), 
+# you can hide data by adding it to gitignore
